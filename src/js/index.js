7fe4f2dd4d5a29cea8a56a4f9d92e98cd7781126
Izabella Loyse <izabellaloyse13@gmail.com>
2021-12-28 23:46:56 -0300

    feat: Consumindo API do Nasa Apod

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
-// AmYKrxfwVwllPmAcQ7vIx7WMxqx4awY0JpM8Va8l
+// eslint-disable-next-line import/extensions
+import APY_KEY from './service.js';
+
+const getDataApod = async () => {
+  try {
+    const response = await fetch(`https://api.nasa.gov/planetary/apod?api_key=${APY_KEY} `);
+    const data = await response.json();
+    console.log(data);
+    return data;
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+
+const loadData = async () => {
+  const description = document.querySelector('.js-explanation');
+  const titleImage = document.querySelector('.js-title-image');
+  const urlImage = document.querySelector('.js-url-image');
+  const urlIframe = document.querySelector('.js-url-iframe');
+
+  const data = await getDataApod();
+  const {
+    explanation, title, url, media_type,
+  } = data;
+
+  if (media_type === 'image') {
+    urlImage.setAttribute('src', url);
+    urlImage.setAttribute('alt', title);
+  } else {
+    urlIframe.setAttribute('src', url);
+    urlIframe.setAttribute('alt', title);
+  }
+
+  description.insertAdjacentHTML('afterbegin', explanation);
+  titleImage.insertAdjacentHTML('afterbegin', title);
+};
+
+loadData();
